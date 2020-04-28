5c334f3679f9097e7b45f389a89b5a252d51dfe2
aydevelop <ay.develop1@gmail.com>
2020-04-28 16:50:16 +0300

    1. User Schema

--- server.js
+++ server.js
@@ -1,27 +1,41 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const app = express();
+require('dotenv').config();
+const User = require("./models/user");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: false} ));
 
-app.get('/', (req, res) => {
+mongoose.connect(process.env.DATABASE,  
+{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}, err => {
+    if(err){ console.log("Error mongoose.connect: " + err); return; }
+    console.log("Successfully connected to database: " + process.env.DATABASE);
+
+    app.listen(process.env.SERVER_PORT, err => {
+        if(err){ console.log("Error app.listen: " + err); return; }
+        console.log("App listening " + process.env.SERVER_PORT + " port");
+    });
+});
+
+app.get("/", (req, res) => {
     res.json("Hello amazon clone");
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body.name);
-    res.end();
-});
+app.post("/", (req, res) => {
+    let user = new User();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password;
 
-
-app.listen(3000, err => {
-    if(err){
-        console.log(err);
-    }
-
-    console.log("listen 3000 ....");
+    user.save(err => {
+        if(err){ res.json(err); }
+        else{ res.json("User saved"); }
+    });
 });
