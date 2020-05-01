1023c372d7be99f0ae671e4198f0eca16ec0e815
aydevelop <ay.develop1@gmail.com>
2020-05-01 13:04:01 +0300

    3. Featured Product

---
+++ client/components/Navbar.vue
@@ -0,0 +1,118 @@
+<template>
+    <div>
+        <header class="">
+            <header class="nav-opt-sprite nav-locate-us nav-lang-en nav-ssl nav-unrec">
+                <div class="container-fluid dekstop-nav">
+                    <div class="row">
+                        <div class="col-sm-2">
+                            <div class="logo-area">
+                                <a href="#">
+                                    <img src="/img/logo.png" alt="Logo" class="img-fluid" >
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 pt-0">
+                            <Search />
+                        </div>
+                        <div class="col-sm-4"></div>
+
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 pl-2">
+                            <div class="nav-global-location">
+                                <nuxt-link to="/address" class="nav-a nav-a-2">
+                                    <div class="nav-sprice" id="nav-packard-glow-loc-icon">
+                                    </div>
+                                    <div class="glow-ingress-block">
+                                        <span id="glow-ingress-line1" class="nav-line-1">
+                                            <i class="fas fa-globe"></i> Deliver to <br><span class="pl-3">California</span>
+                                        </span>
+                                    </div>
+                                </nuxt-link>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-5 col-md-4 col-sm-6 p-0">
+                            <div class="nav-fill">
+                                <div class="nav-shop">
+                                    <nuxt-link to="/history" class="nav-a nav-a-2 nav-single-row-link">
+                                        <span class="nav-line-2">
+                                            Browsing History
+                                            <span style="visability: visible" class="nav-icon nav-arrow">
+
+                                            </span>
+                                        </span>
+                                    </nuxt-link>
+                                </div>
+                                <div class="nav-xshop-container">
+                                    <div class="nav-xshop">
+                                        <a href="#" class="nav-a">Today's deals</a>
+                                        <a href="#" class="nav-a">Your amazon.com</a>
+                                        <a href="#" class="nav-a">Gift cards</a>
+                                        <a href="#" class="nav-a">Registery</a>
+                                        <a href="#" class="nav-a">Sell</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-4 p-0">
+                            <div class="nav-tools">
+                                <a href="#" 
+                                class="nav-a nav-a-2 icp-link-style-2"
+                                id="icp-nav-flyout">
+                                    <span class="icp-nav-link-inner">
+                                        <span class="nav-line-1">
+                                            <span class="icp-nav-globe-img-2"></span>
+                                            <span class="icp-nav-language">EN</span>
+                                        </span>
+                                        
+                                        <span class="ml-2 nav-line-2">                                            
+                                            <span style="visability: visible"
+                                            class="nav-icon nav-arrow">
+                                            </span>
+                                        </span>
+                                    </span>
+                                </a>
+                                <!-- <span class="icp-nav-link-border"></span> -->
+                                <nuxt-link to="/register" id="nav-link-accountList"
+                                    tabindex="0" class="nav-a nav-a-2">
+                                    <span class="nav-line-1" >Hello, Sign in</span>
+                                    <span class="nav-line-2">
+                                        Account &amp; Lists
+                                        <span class="nav-icon nav-arrow" 
+                                        style="visability: visible" ></span>
+                                    </span>
+                                </nuxt-link>
+                                <nuxt-link to="/orders" class="nav-a nav-a-2 nav-single-row-link">
+                                    <span class="nav-line-1"></span>
+                                    <span class="nav-line-2">Orders</span>
+                                </nuxt-link>
+                                <nuxt-link to="/cart" class="nav-a nav-a-2 nav-single-row-link">
+                                    <span class="nav-line-1"></span>
+                                    <span class="nav-line-2">
+                                    <i class="fas fa-shopping-basket"></i> Cart (10)</span>
+                                </nuxt-link>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </header>
+        </header>
+    </div>
+</template>
+
+<script>
+import Search from "~/components/Search";
+
+export default {
+  components: {
+    Search
+  }
+}
+</script>
+
+<style>
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,72 +1,27 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        client
-      </h1>
-      <h2 class="subtitle">
-        My laudable Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <main class="listingPage">
+    <div class="counter-fluid">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
+          sidebar
+        </div>
+        <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+          <FeaturedProduct />
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import FeaturedProduct from '~/components/FeaturedProduct.vue'
 
 export default {
   components: {
-    Logo
+    FeaturedProduct
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
